e4e61fd099edfcfdd7f6b0d9a8a03cb0aa7cff28
Igor <igormgg@hotmail.com>
2021-09-22 13:22:54 -0300

    feat: Register and Login forms

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,34 @@
+import { Button, TextField } from "@material-ui/core";
+import { useHistory } from "react-router";
+
 const Login = () => {
+  const history = useHistory();
+
+  const redirectTo = (path) => {
+    history.push(path);
+  };
+
   return (
     <div>
-      <h1>login</h1>
+      <div>
+        <TextField
+          label="Digite seu email"
+          variant="standard"
+          size="small"
+          color="secondary"
+          margin="dense"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Digite sua senha"
+          variant="standard"
+          size="small"
+          color="secondary"
+          margin="dense"
+        />
+      </div>
+      <Button onClick={() => redirectTo("/")}>Voltar à página inicial</Button>
     </div>
   );
 };
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,8 +1,80 @@
+import { Button, TextField } from "@material-ui/core";
+import { useHistory } from "react-router";
+
 const Register = () => {
+  const history = useHistory();
+
+  const redirectTo = (path) => {
+    history.push(path);
+  };
+
   return (
-    <div>
-      <h1>register</h1>
-    </div>
+    <form>
+      <div>
+        <TextField
+          label="Email de preferência"
+          variant="standard"
+          size="small"
+          color="warning"
+          margin="dense"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Crie uma senha forte"
+          variant="standard"
+          size="small"
+          color="warning"
+          margin="dense"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Confirme sua senha"
+          variant="standard"
+          size="small"
+          color="warning"
+          margin="dense"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Qual é o seu nome?"
+          variant="standard"
+          size="small"
+          color="warning"
+          margin="dense"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Digite algo sobre você"
+          variant="standard"
+          size="small"
+          color="warning"
+          margin="dense"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Seu número de telefone"
+          variant="standard"
+          size="small"
+          color="warning"
+          margin="dense"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Qual módulo você cursa?"
+          variant="standard"
+          size="small"
+          color="warning"
+          margin="dense"
+        />
+      </div>
+      <Button onClick={() => redirectTo("/")}>Voltar à página inicial</Button>
+    </form>
   );
 };
 
